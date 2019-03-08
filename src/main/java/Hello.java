a34bce23da0e0fff3035a6de5a7f6eae8700c9bf
pgp <pgp@gmx.it>
2019-03-08 21:00:56 +0100

    first commit after fork

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,10 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world!";
+  public static String GREETING = "23571113171923";
   
   public static void main(String []args) {
-    
+    System.out.println(1234567890);
   }  
 }
 
---
+++ src/main/java/it/pgp/jitpackexample/SampleClass.java
@@ -0,0 +1,16 @@
+package it.pgp.jitpackexample;
+
+public class SampleClass {
+
+    public int a;
+    public float b;
+
+    public SampleClass(int a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String getString() {
+        return "abc";
+    }
+}
